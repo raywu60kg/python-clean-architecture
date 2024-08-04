2d972a698ee3b30ad5abdc4689dc5446d1e7634b
ray <wuhaohsiang1992@gmail.com>
2024-08-04 15:25:05 +0800

    update controller

--- src/adapter/inward/web/get_account_balance/get_account_balance_controller.py
+++ src/adapter/inward/web/get_account_balance/get_account_balance_controller.py
@@ -1,24 +1,17 @@
 from fastapi import APIRouter, Path
 
-from .....application.domain.entity.account import AccountId
-from .....application.domain.entity.money import Money
-from .....application.port.inward.send_money.send_money_command import SendMoneyCommand
-from .....application.port.inward.send_money.send_money_use_case import SendMoneyUseCase
+from src.application.domain.entity.account import AccountId
+from src.application.port.inward.get_account_balance.get_account_balance_query import GetAccountBalanceQuery
+from src.application.port.inward.get_account_balance.get_account_balance_use_case import GetAccountBalanceUseCase
 
 router = APIRouter()
 
 
-@router.post("/accounts/send/{source_account_id}/{target_account_id}/{amount}")
-async def send_money(
-    source_account_id: int = Path(..., description="The ID of the source account"),
-    target_account_id: int = Path(..., description="The ID of the target account"),
-    amount: int = Path(..., description="The amount of money to send"),
-    send_money_use_case: SendMoneyUseCase,
-):
-    command = SendMoneyCommand(
-        source_account_id=AccountId(value=source_account_id),
-        target_account_id=AccountId(value=target_account_id),
-        money=Money(value=amount),
-    )
-    send_money_use_case.send_money(command=command)
-    return
+@router.get("/accounts/balance/{owner_account_id}/")
+async def get_account_balance(
+    get_account_balance_use_case: GetAccountBalanceUseCase,
+    owner_account_id: int = Path(..., description="The ID of the owner account"),
+) -> int:
+    query = GetAccountBalanceQuery(account_id=AccountId(value=owner_account_id))
+    balance = await get_account_balance_use_case.get_account_balance(query=query)
+    return balance.amount
